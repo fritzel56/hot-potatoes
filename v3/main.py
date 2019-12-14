0a60bb66ba6d747756e42a2dc4d61e5fe128e310
fritzel56 <20907117+fritzel56@users.noreply.github.com>
2019-12-14 12:18:06 -0500

    adding main function, email sending, exception composer, data writer

--- v3/main.py
+++ v3/main.py
@@ -61,3 +61,107 @@ def get_bq_data():
     query = query.format('`'+project_id+'.'+dataset+'.'+tablename+'`')
     client = bigquery.Client()
     return client.query(query).result().to_dataframe()
+
+
+def write_to_gbq(ticker, data, most_recent):
+    """Takes in a dataframe and writes the values to BQ
+    Args:
+        stocks_df(df): the dataframe to be written
+    """
+    latest = most_recent.loc[most_recent.ticker==ticker, 'snap_date']
+    if len(latest)==0:
+        to_write = data
+    else:
+        to_write = data.loc[data.Date2>latest[0]]
+    dataset = os.environ['DATASET']
+    tablename = os.environ['TABLENAME']
+    # set up connection details
+    client = bigquery.Client()
+    dataset_ref = client.dataset(dataset)
+    table_ref = bigquery.TableReference(dataset_ref, tablename)
+    table = client.get_table(table_ref)
+    # convert to list of lists
+    rows_to_insert = to_write.values.tolist()
+    # write data
+    errors = client.insert_rows(table, rows_to_insert)
+    assert errors == []
+
+
+def id_new_data():
+    """Queries BQ for the most recent entry for each ETF
+    Returns:
+        old_df: the
+    """
+
+def error_composition(e):
+    """Composes an email in the event of an exception with exception details.
+    Args:
+        e(Exception): the exception which was raised
+    Returns:
+        dict: data structure containing the composed email ready for MJ's API
+    """
+    contact_email = os.environ['contact_email']
+    contact_name = os.environ['contact_name']
+    data = {
+      'Messages': [
+        {
+          "From": {
+            "Email": contact_email,
+            "Name": contact_name
+          },
+          "To": [
+            {
+              "Email": contact_email,
+              "Name": contact_name
+            }
+          ],
+          "Subject": "There was an error with the hot potatoes run",
+          "HTMLPart": "There was an error with the hot potatoes run: {}".format(' '.join(e.args)),
+        }
+      ]
+    }
+    return data
+
+
+def send_email(email):
+    """Takes in a composed email and sends it using the mailjet api
+    Args:
+        email(dict): dict containing all relevant fields needed by the mailjet API
+    """
+    api_key = os.environ['api_key']
+    api_secret = os.environ['api_secret']
+    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+    result = mailjet.send.create(data=email)
+
+
+def kickoff(request):
+    """Function which orchestrates the rest of the code
+    Args:
+        request: passed as part of the Google Function orchestration service. Not used.
+    """
+    with open('stocks.yaml') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+    # tickers for which we want reports
+    tickers = data['tickers']
+    # dictionary connecting tickers to readible names. Used in email.
+    name_mapping = data['mapping']
+    try:
+        most_recent = get_bq_data()
+        for ticker in tickers:
+            data = get_daily_data(ticker)
+            write_to_gbq(ticker, data, most_recent)
+    except Exception as e:
+        email = error_composition(e)
+        send_email(email)
+
+
+
+        pct_df = pd.DataFrame(list(pct.items()), columns=['etf', 'return'])
+        if not stock_change(pct_df):
+            email = compose_summary_email(pct, name_mapping)
+            send_email(email)
+        pct_df = prep_data(pct_df)
+        write_to_gbq(pct_df)
+    except Exception as e:
+        email = error_composition(e)
+        send_email(email)
